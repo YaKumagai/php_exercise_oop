cf2e0188bae19e9f31167a0f46fb7aa4ad6334e1
YaKumagai <yasuo.kumagai@kameyanet.co.jp>
2021-02-14 19:05:54 +0900

    second commit

--- index.php
+++ index.php
@@ -1,47 +1,23 @@
 <?php
-
-// 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
-
-
-
-
-
-// 以下を実行した時にエラーがでないようにして下さい。
-// Carクラスのインスタンスを生成
-// 引数 : 車名、車体番号、カラー
-$car1 = new Car('ランクル', 100, 'black');
-
-// 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->information();
-echo '<br>';
-
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->information();
-echo '<br>';
+class Car {
+    public $getName;
+    public $getNumber;
+    public $getColor;
+    public function __construct($getName, $getNumber, $getColor)
+    {
+        $this->getName = $getName;
+        $this->getNumber = $getNumber;
+        $this->getColor = $getColor;
+    }
+    public function selfIntroduction()
+    {
+        echo $this->getName . '<br>';
+        echo $this->getNumber . '<br>';
+        echo $this->getColor . '<br>';
+        echo '車の車種：' . $this->getName . '、車体番号：' . $this->getNumber . '、カラー：' . $this->getColor . 'です。<br>';
+    }
+}
+$car = new Car('ランクル', 100, 'black');
+$car->selfIntroduction();
+$car = new Car('アルファード', 200, 'red');
+$car->selfIntroduction();
